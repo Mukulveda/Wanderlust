7ccba2f3bf40e92758cd4477aaf8fd9fc6ea34ec
mukjain33 <juug22btech56592@gmail.com>
2025-03-30 13:13:44 +0530

    Search based on location

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,24 +1,33 @@
 const Listing = require("../models/listing");
 const Review = require("../models/review");
 
-module.exports.createReview = async(req,res)=>{
- let listing = await Listing.findById(req.params.id);
- let newReview = new Review (req.body.review);
- console.log(newReview);
- newReview.author= req.user._id;
- listing.reviews.push(newReview);
+module.exports.createReview = async (req, res) => {
+    let listing = await Listing.findById(req.params.id).populate("reviews");
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
 
- await newReview.save();
- await listing.save();
- req.flash("success","New Review Created");
- res.redirect(`/listings/${listing._id}`);
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    req.flash("success", "New Review Created");
+    res.redirect(`/listings/${listing._id}`);
 };
 
-module.exports.destroyReview = async(req,res)=>{
-  let {id,reviewId} = req.params;
+module.exports.destroyReview = async (req, res) => {
+    let { id, reviewId } = req.params;
+    let listing = await Listing.findById(id);
+    let review = await Review.findById(reviewId);
 
-  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  req.flash("success","Review Deleted");
-  res.redirect(`/listings/${id}`);
+    // Check if the user is the review author or the listing owner
+    if (review.author.equals(req.user._id) || listing.owner.equals(req.user._id)) {
+        await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+        await Review.findByIdAndDelete(reviewId);
+        req.flash("success", "Review Deleted");
+    } else {
+        req.flash("error", "You do not have permission to delete this review.");
+    }
+
+    res.redirect(`/listings/${id}`);
 };
--- routes/review.js
+++ routes/review.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const router = express.Router({mergeParams:true });
+const router = express.Router({ mergeParams: true });
+
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js");
-const {validateReview,isLoggedIn, isReviewAuthor} = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 const reviewController = require("../controllers/reviews.js");
 
-//Post Review Route
-router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview));
+// Post Review Route
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
+// Delete Review Route (Only Review Author or Listing Owner can delete)
+router.delete("/:reviewId", isLoggedIn, wrapAsync(reviewController.destroyReview));
 
-//Delete Review Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
-
-module.exports= router;
+module.exports = router;
